bccc80780acf4b305c9ad6d10fffd1d1269d8e3a
Rednata <maknatosh@gmail.com>
2023-04-03 10:08:01 +0700

    feat: burger interactive

---
+++ shelter/src/script/modules/burger.js
@@ -0,0 +1,24 @@
+console.log('111111111111');
+
+export const controlBurger = () => {
+  const btnMenu = document.querySelector('.menu-btn');  
+  const menu = document.querySelector('.burger'); 
+  const overlay = document.querySelector('.overlay');  
+
+  btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('burger_active');
+    overlay.classList.toggle('overlay_active');
+    btnMenu.classList.toggle('menu-btn_active');    
+  })
+
+  const linkMenu = document.querySelectorAll('.burger .nav__link');
+  linkMenu.forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.toggle('burger_active');
+    overlay.classList.toggle('overlay_active');
+    btnMenu.classList.toggle('menu-btn_active');    
+    })
+  })
+
+
+}
